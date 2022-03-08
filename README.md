855fb2576493e4d10a0872288ab39baba52c6d5d
Gabriel S <sirlopug@gmail.com>
2022-03-07 22:46:21 -0800

    created simple test and initial lottery contract

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Decentralized Lottery App
+
 1. Users can enter lotter with ETH based on a USD fee
 2. An admin will choose when the lottery is over (e.g. DAO or Chainlink Keepers)
 3. The lottery will select a random winner
+   
+To test:
+
+1. `mainnet-fork`
+2. `development` with mocks
+3. `testnet`
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,11 +2,64 @@
 
 pragma solidity ^0.6.6;
 
-contract Lottery {
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    function enter() public {}
-    function getEntranceFee() public{}
-    function startLottery() public {}
-    function endLottery() public{}
-    
+contract Lottery is Ownable {
+
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN, 
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+    // 0
+    // 1
+    // 2
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+    }
+
+    function enter() public payable {
+        //$50 min
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256){
+        (,int256 price,,,) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+        // $50, $2000 /ETH
+        // 50/2,000
+        // 50 * 100000 / 2000
+        uint256 constToEnter = (usdEntryFee *10 ** 18) / adjustedPrice;
+        return constToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(lottery_state == LOTTERY_STATE.CLOSED, "Can't start a new lottery yet!");
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {
+        //not secure for production
+        // uint256(
+        //     keccack256(
+        //         abi.encodePacked(
+        //             nonce, //none is predictable (aka, trx #)
+        //             msg.sender, // msg.sender is predictable
+        //             block.difficulty, // can be manipulated by miners
+        //             block.timestamp // timestamp is predictable
+        //         )
+        //     )
+        // ) % players.length;
+    }
+
 }
