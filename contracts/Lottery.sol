76786011ebbd7df8ba9ed3f1a0e3c5e5f424b769
Gabriel S <sirlopug@gmail.com>
2022-03-07 21:54:40 -0800

    initial

---
+++ contracts/Lottery.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+contract Lottery {
+
+    function enter() public {}
+    function getEntranceFee() public{}
+    function startLottery() public {}
+    function endLottery() public{}
+    
+}
